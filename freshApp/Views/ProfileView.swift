10a3582f3eb644db5f550784659453b9cf601f6f
Trevor <tbuhler347@gmail.com>
2021-09-22 19:52:07 -0600

    fg

--- freshApp/Views/ProfileView.swift
+++ freshApp/Views/ProfileView.swift
@@ -8,15 +8,38 @@
 import SwiftUI
 
 struct ProfileView: View {
+    
+    @State private var posts: [String] = ["sadg", "asdg", "kfk", "hyf", "jkk"]
+    
     var body: some View {
         ZStack {
             Color.theme.background
                 .ignoresSafeArea()
-            VStack {
+            ScrollView {
                 Rectangle()
                     .frame(maxWidth: .infinity)
                     .frame(height: 50)
+                    .foregroundColor(Color.theme.pinkColor)
+                    .onTapGesture {
+                        // navigate to the file we post.
+                    }
+                    .overlay(
+                        Text("Add Post")
+                            .foregroundColor(.white)
+                    )
+                Spacer()
+                ForEach(posts, id: \.self) { post in
+                    HStack {
+                        Rectangle()
+                            .frame(width: 120, height: 120)
+                        
+                        Text("This will be the title of whatever the title screen is.")
+                            .font(.body)
+                            .multilineTextAlignment(.center)
+                    }
+                }
             }
+            .padding()
         }
     }
 }
@@ -24,5 +47,6 @@ struct ProfileView: View {
 struct ProfileView_Previews: PreviewProvider {
     static var previews: some View {
         ProfileView()
+            .preferredColorScheme(.dark)
     }
 }
