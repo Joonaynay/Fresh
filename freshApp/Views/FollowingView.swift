009d0b1211eba8bfecba272e7960d2faf9d2f618
Joonaynay <Joonaynay@gmail.com>
2021-09-22 19:37:44 -0600

    TabView

---
+++ freshApp/Views/FollowingView.swift
@@ -0,0 +1,20 @@
+//
+//  FollowingView.swift
+//  freshApp
+//
+//  Created by Forrest Buhler on 9/22/21.
+//
+
+import SwiftUI
+
+struct FollowingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FollowingView_Previews: PreviewProvider {
+    static var previews: some View {
+        FollowingView()
+    }
+}
