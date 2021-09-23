893c0317ab78965ff25c8df72b750490583ec7e6
Trevor <tbuhler347@gmail.com>
2021-09-22 18:46:00 -0600

    Initial Commit

---
+++ freshApp/freshAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  freshAppApp.swift
+//  freshApp
+//
+//  Created by Wendy Buhler on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct freshAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
