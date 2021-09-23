eb0810034fa7000a899339c7f8bc4e85715ca0de
Joonaynay <Joonaynay@gmail.com>
2021-09-22 19:22:20 -0600

    HomePostView

--- freshApp/Views/HomeView.swift
+++ freshApp/Views/HomeView.swift
@@ -9,21 +9,56 @@ import SwiftUI
 
 struct HomeView: View {
     
-    let subjects = SubjectsModel()
+    
+    @State var destination = ""
+    @State private var selection: String? = ""
+    
+    @State private var subject = ""
+    private let homepostview = "homepostview"
+    private let subjects = SubjectsModel()
+    
+    
     
     var body: some View {
-        VStack {
-            ForEach(subjects.list, id: \.self) { subject in
-                Button(subject) {}
-                    .padding()
-                    .background(Color.pink)
-            }
-        }
-    }
-}
+        NavigationView {
+            ZStack {
+                NavigationLink(
+                    destination: HomePostView(subject: subject),
+                    tag: homepostview,
+                    selection: $selection,
+                    label: {})
+                Color.theme.background
+                    .ignoresSafeArea()
+                ScrollView {
+                    VStack {
+                        ForEach(subjects.list, id: \.self) { subject in
+                            Button(action: {
+                                self.subject = subject
+                                selection = homepostview
+                            }, label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .padding()
+                                        .frame(maxWidth: .infinity)
+                                        .frame(height: 100)
+                                        .foregroundColor(Color.theme.pinkColor)
+                                    Text(subject)
+                                }
+                            }) // Button
+                        } // ForEach
+                    } // VStack
+                } // ScrollView
+                .navigationTitle("Subjects")
+            } // ZStack
+        } // NavigationView
+    } // Body
+} // View
+
+
 
 struct HomeView_Previews: PreviewProvider {
     static var previews: some View {
         HomeView()
+            .preferredColorScheme(.dark)
     }
 }
