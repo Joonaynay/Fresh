854c3df0bdba1473d737dfd7e07acb756b11a62f
Trevor <tbuhler347@gmail.com>
2021-09-22 18:51:16 -0600

    start

---
+++ freshApp/Views/LoginView.swift
@@ -0,0 +1,20 @@
+//
+//  LoginView.swift
+//  freshApp
+//
+//  Created by Wendy Buhler on 9/22/21.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
