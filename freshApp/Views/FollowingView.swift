58fdaa6bd46c9a3fcc642c2d1b12c99d12af4c71
Trevor <tbuhler347@gmail.com>
2021-09-24 18:01:54 -0600

    jkl

--- freshApp/Views/FollowingView.swift
+++ freshApp/Views/FollowingView.swift
@@ -9,11 +9,35 @@ import SwiftUI
 
 struct FollowingView: View {
     
+    @EnvironmentObject var fb: FirebaseModel
+    
     var body: some View {
         ZStack(alignment: .topLeading) {
+            Color.theme.background
+                .ignoresSafeArea()
             VStack(spacing: 0) {
+                Button(action: { fb.loadPosts() }, label: {
+                    Text("Load")
+                })
+                
                 TitleBarView(title: "Following")
-                ScrollView {}
+                ScrollView {
+                        VStack {
+                            ForEach(fb.posts) { post in
+                                Image(uiImage: post.image!)
+                                    .resizable()
+                                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width)
+                                HStack {
+                                    Image(systemName: "person.circle.fill")
+                                        .resizable()
+                                        .frame(width: 50, height: 50)
+                                    Text("Username: \(post.caption)")
+                                        .multilineTextAlignment(.leading)
+                                        .font(.title3)
+                                }.padding(25)
+                            }
+                    }
+                }
             }
         }
         .navigationBarHidden(true)
@@ -23,5 +47,6 @@ struct FollowingView: View {
 struct FollowingView_Previews: PreviewProvider {
     static var previews: some View {
         FollowingView()
+            .environmentObject(FirebaseModel())
     }
 }
