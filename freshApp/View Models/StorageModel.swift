5748525f9d279b41514302f1961ff7733a853e9d
Joonaynay <Joonaynay@gmail.com>
2021-09-28 16:50:31 -0600

    Storage

--- freshApp/View Models/StorageModel.swift
+++ freshApp/View Models/StorageModel.swift
@@ -1,29 +1,37 @@
 //
-//  FirebaseStorageModel.swift
+//  StorageModel.swift
 //  freshApp
 //
-//  Created by Forrest Buhler on 9/27/21.
+//  Created by Forrest Buhler on 9/28/21.
 //
 
 import SwiftUI
 import FirebaseStorage
 
-struct FirebaseStorageModel {
-    @EnvironmentObject var fb: FirebaseModel
-    
-    let storage = Storage.storage().reference()
+extension FirebaseModel {
     
     func saveImage(path: String, file: String, image: UIImage) {
+        //Convert UIImage to Data
         let imageData = image.jpegData(compressionQuality: 1)
+        
+        //Save data to FirebaseStorage
         storage.child(path).child(file).putData(imageData!)
     }
     
     func loadImage(path: String, id: String, completion:@escaping (UIImage?) -> Void) {
+        
+        //Load Image Data
         storage.child(path).child(id).getData(maxSize: 20 * 1024 * 1024) { data, error in
+            
+            //Check for error
             if error == nil {
+                
+                //Convert data to UIImage then return the image
                 let image = UIImage(data: data!)
                 completion(image)
             } else {
+                
+                //Return Nil
                 completion(nil)
             }
         }
